7e8e8fbb3d47098684f974210c7ef3d864bcdfc1
Borys Prydatko <bpridatko@gmail.com>
2022-11-03 10:21:32 +0000

    Finished + extension

--- Program.cs
+++ Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("How many numbers you want to input: ");
+int amountOfNumbers = 1;
+float result = 0;
+bool invalidinput = true;
+while (!int.TryParse(Console.ReadLine(), out amountOfNumbers))
+{
+    Console.Write("Invalid input. Please enter one more time: ");
+}
+float[] arr = new float[amountOfNumbers];
+for (int i = 0; i < amountOfNumbers; i++)
+{
+    while (invalidinput == true)
+    {
+        try
+        {
+            Console.Write("Input your " + (i + 1) + " number ");
+            arr[i] = float.Parse(Console.ReadLine());
+            result += arr[i];
+            break;
+        }
+        catch
+        {
+            Console.Write("Invalid input. ");
+        }
+    }
+}
+result /= amountOfNumbers;
+Console.WriteLine("Your number is " + result);
+Console.ReadLine();
